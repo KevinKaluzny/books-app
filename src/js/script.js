f7eba3637d96e60e3724b3788d2b9561a38ad789
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-05-12 09:16:23 +0200

    Make constant 'booksList'

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ function renderBooks() {
     for (let data in dataSource.books) {
         const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
         const html = template(dataSource.books[data]);
+        const booksList = document.querySelector('.books-list');
     }
 }
 
