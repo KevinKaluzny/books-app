757bac50ef438962acdebd0bd49baec3ece5070d
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-05-12 09:14:12 +0200

    Generate html by function 'template'

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 function renderBooks() {
     for (let data in dataSource.books) {
         const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
+        const html = template(dataSource.books[data]);
     }
 }
 
