790522dc621bd427992b6b6c0f0c52edec978662
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-05-12 10:17:45 +0200

    Make empty array 'favoriteBooks'

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+const favoriteBooks = [];
+
 function renderBooks() {
     for (let data in dataSource.books) {
         const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
